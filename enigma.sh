3a71c75cd34eca3eca7fc7201369389ca3414d2e
Maksim Melnikov <max@sopka.ru>
2024-12-28 23:29:58 +0300

    Strip nested folders while untaring

--- enigma.sh
+++ enigma.sh
@@ -352,8 +352,8 @@ decrypt_files() {
         tree
 
         path_to_output="${OUTPUT_DIR}"
-        show_logs 4 "Running tar -xzf ${path_to_tar} -C ${path_to_output}"
-        tar -xzf $path_to_tar -C $path_to_output
+        show_logs 4 "Running tar -xzf ${path_to_tar} -C ${path_to_output} -strip-components=1"
+        tar -xzf $path_to_tar -C $path_to_output -strip-components=1
 
         tree
 
